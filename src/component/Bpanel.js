5f9bdd1ffb63fefb7377735db8a46748ee361770
addo <forison6205@gmail.com>
2019-12-12 17:39:41 -0500

    added: more styling to gameboard

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -4,22 +4,65 @@ class Bpanel extends Component {
 	render() {
 		return (
 			<div className="container">
-				<div className="row">
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-6"></div>
+				<div className="row ">
+					<div className="col-3">
+						<button className="btn btn-success">AC</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">+/-</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">%</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">/</button>
+					</div>
+
+					<div className="col-3">
+						<button className="btn btn-success">1</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">2</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">3</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">*</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">4</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">5</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">6</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">-</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">7</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">8</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">9</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">+</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">0</button>
+					</div>
+					<div className="col-3">
+						<button className="btn btn-success">.</button>
+					</div>
+					<div className="col-6 p-0">
+						<button className="btn btn-success">=</button>
+					</div>
 				</div>
 			</div>
 		)
