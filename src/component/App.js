2be54056deff933afda138d4477efee518ccce94
addo <forison6205@gmail.com>
2019-12-12 13:47:24 -0500

    added: directory structure preparation

--- src/component/App.js
+++ src/component/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header" />
+    <div className="App text-center">
+      hello world
     </div>
   );
 }
