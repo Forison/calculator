66546254f0bfc71ccfc52f284d6ef7a8537b086f
addo <forison6205@gmail.com>
2019-12-12 16:47:04 -0500

    added: layout of calculator panel

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 class Bpanel extends Component {
 	render() {
 		return (
-			<div className="butBoard">
-				hello
-      </div>
+			<div className="container">
+				<div className="row">
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-3"></div>
+					<div className="col-6"></div>
+				</div>
+			</div>
 		)
 	}
 }
