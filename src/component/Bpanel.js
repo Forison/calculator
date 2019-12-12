1bf9bed2f1420ee03fcdacfbe3dccf0e9d2f8a89
addo <forison6205@gmail.com>
2019-12-12 18:36:25 -0500

    added: refactored styling for cal board

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -6,62 +6,62 @@ class Bpanel extends Component {
 			<div className="container">
 				<div className="row ">
 					<div className="col-3">
-						<button className="btn btn-success">AC</button>
+						<button className="btn btn-secondary">AC</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">+/-</button>
+						<button className="btn btn-secondary">+/-</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">%</button>
+						<button className="btn btn-secondary">%</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">/</button>
+						<button className="btn btn-secondary">/</button>
 					</div>
 
 					<div className="col-3">
-						<button className="btn btn-success">1</button>
+						<button className="btn btn-secondary">1</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">2</button>
+						<button className="btn btn-secondary">2</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">3</button>
+						<button className="btn btn-secondary">3</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">*</button>
+						<button className="btn btn-secondary">*</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">4</button>
+						<button className="btn btn-secondary">4</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">5</button>
+						<button className="btn btn-secondary">5</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">6</button>
+						<button className="btn btn-secondary">6</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">-</button>
+						<button className="btn btn-secondary">-</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">7</button>
+						<button className="btn btn-secondary">7</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">8</button>
+						<button className="btn btn-secondary">8</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">9</button>
+						<button className="btn btn-secondary">9</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">+</button>
+						<button className="btn btn-secondary">+</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">0</button>
+						<button className="btn btn-secondary">0</button>
 					</div>
 					<div className="col-3">
-						<button className="btn btn-success">.</button>
+						<button className="btn btn-secondary">.</button>
 					</div>
 					<div className="col-6 p-0">
-						<button className="btn btn-success">=</button>
+						<button className="btn btn-secondary">=</button>
 					</div>
 				</div>
 			</div>
