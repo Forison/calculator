d9e89b5cca3615b36ad1d4e32d02efcfb5dc92ef
addo <forison6205@gmail.com>
2019-12-13 06:16:48 -0500

    linter error

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -2,68 +2,68 @@ import React from 'react';
 
 const Bpanel = () => (
 
-	<div className="container">
-		<div className="row ">
-			<div className="col-3">
-				<button type="button" className="btn btn-success">AC</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-success">+/-</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-success">%</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-success">/</button>
-			</div>
+  <div className="container">
+    <div className="row ">
+      <div className="col-3">
+        <button type="button" className="btn btn-success">AC</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-success">+/-</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-success">%</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-success">/</button>
+      </div>
 
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">1</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">2</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">3</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-success">*</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">4</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">5</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">6</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-success">-</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">7</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">8</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">9</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-success">+</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">0</button>
-			</div>
-			<div className="col-3">
-				<button type="button" className="btn btn-secondary">.</button>
-			</div>
-			<div className="col-6 p-0">
-				<button type="button" className="btn btn-warning">=</button>
-			</div>
-		</div>
-	</div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">1</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">2</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">3</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-success">*</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">4</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">5</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">6</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-success">-</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">7</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">8</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">9</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-success">+</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">0</button>
+      </div>
+      <div className="col-3">
+        <button type="button" className="btn btn-secondary">.</button>
+      </div>
+      <div className="col-6 p-0">
+        <button type="button" className="btn btn-secondary">=</button>
+      </div>
+    </div>
+  </div>
 );
 
 export default Bpanel;
