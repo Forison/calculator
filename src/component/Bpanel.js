7d83dfaa717e54ebd471b30fc4b5a134c30696bd
addo <forison6205@gmail.com>
2019-12-16 09:07:30 -0500

    added: button as required by specification on milestone 2

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -3,7 +3,7 @@ import Button from './Button';
 
 const Bpanel = () => (
   <div>
-		<Button/>
+    <Button />
   </div>
 );
 
--- src/component/Button.js
+++ src/component/Button.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Button = ({ name }) => {
-  return (
-    <button>
-      {name}
-    </button>
-  );
-}
+const Button = ({ name }) => (
+  <button type="button">
+    {name}
+  </button>
+);
 Button.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string.isRequired,
 };
 
 export default Button;
