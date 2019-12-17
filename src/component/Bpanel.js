5709cf61c18a38a56f3da137c73722e2f9950cdf
addo <forison6205@gmail.com>
2019-12-17 10:02:14 -0500

    added changes

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -2,7 +2,8 @@ import React from 'react';
 import id from 'shortid';
 import Button from './Button';
 
-const zeroStyler = arg => (arg === '0' ? 'col-6' : 'col-3');
+const wide = (arg) => (arg === '0' ? 'wide' : 'normal');
+const zeroStyling = (arg) => (arg === '0' ? 'col-6' : 'col-3');
 const colorStyler = () => ('colorMe');
 const Bpanel = () => {
   const butName = [
@@ -15,13 +16,13 @@ const Bpanel = () => {
   return (
     <div>
       {
-        butName.map(group => (
+        butName.map((group) => (
           <div key={id.generate()}>
             <div className="container">
               <div className="row">
-                {group.map(ButtonText => (
-                  <div key={id.generate()} className={`${zeroStyler(ButtonText)}`}>
-                    <Button name={ButtonText} color={colorStyler()} />
+                {group.map((ButtonText) => (
+                  <div key={id.generate()} className={`${zeroStyling(ButtonText)}`}>
+                    <Button name={ButtonText} color={colorStyler()} wide={`${wide(ButtonText)}`} />
                   </div>
                 ))}
               </div>
