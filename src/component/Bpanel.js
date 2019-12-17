3bc9a7274278b67aa3c2be3cd9601680a0c31407
addo <forison6205@gmail.com>
2019-12-17 10:04:43 -0500

    fix: linter error

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -2,8 +2,8 @@ import React from 'react';
 import id from 'shortid';
 import Button from './Button';
 
-const wide = (arg) => (arg === '0' ? 'wide' : 'normal');
-const zeroStyling = (arg) => (arg === '0' ? 'col-6' : 'col-3');
+const wide = arg => (arg === '0' ? 'wide' : 'normal');
+const zeroStyling = arg => (arg === '0' ? 'col-6' : 'col-3');
 const colorStyler = () => ('colorMe');
 const Bpanel = () => {
   const butName = [
@@ -16,11 +16,11 @@ const Bpanel = () => {
   return (
     <div>
       {
-        butName.map((group) => (
+        butName.map(group => (
           <div key={id.generate()}>
             <div className="container">
               <div className="row">
-                {group.map((ButtonText) => (
+                {group.map(ButtonText => (
                   <div key={id.generate()} className={`${zeroStyling(ButtonText)}`}>
                     <Button name={ButtonText} color={colorStyler()} wide={`${wide(ButtonText)}`} />
                   </div>
