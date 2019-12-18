4703c161aa34f18d4d8034cf5a8f64f4a6211d80
addo <forison6205@gmail.com>
2019-12-18 06:29:48 -0500

    added: Implementation of  calculate.js module

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App';
+import calculate from './logic/calculate';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+const calObj = {
+    total: 8,
+    next: 9,
+    operation: '+'
+}
+console.log(calculate(calObj, 'x'));
---
+++ src/logic/calculate.js
@@ -0,0 +1,34 @@
+// import React from 'react';
+
+const calculate = ({ total, next, operation }, butName) => {
+	const calObj = {
+		total: total,
+		next: next,
+		operation: operation
+	}
+	switch (butName) {
+		case 'AC':
+			calObj.total = '';
+			calObj.next = '';
+			calObj.operation = '';
+			break;
+		case '+/-':
+			calObj.total = calObj.total * -1;
+			calObj.next = calObj.next * -1;
+			break;
+		case '+':
+		case '-':
+		case 'x':
+		case '÷':
+		case '%':
+			calObj.operation = butName;
+			break;
+		case '=':
+			return total;
+
+		default:
+			return calObj
+	}
+	return calObj;
+}
+export default calculate;
