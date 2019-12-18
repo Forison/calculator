b293edd4dae7333e80b93ae211ae57fd5b206b0f
addo <forison6205@gmail.com>
2019-12-18 08:47:06 -0500

    added: calculate module to app component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './component/App';
-import operate from './logic/operate';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-console.log(operate(1, 7,'÷'));
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,32 +1,32 @@
 const calculate = ({ total, next, operation }, butName) => {
-	const calObj = {
-		total: total,
-		next: next,
-		operation: operation
-	}
-	switch (butName) {
-		case 'AC':
-			calObj.total = '';
-			calObj.next = '';
-			calObj.operation = '';
-			break;
-		case '+/-':
-			calObj.total = calObj.total * -1;
-			calObj.next = calObj.next * -1;
-			break;
-		case '+':
-		case '-':
-		case 'x':
-		case '÷':
-		case '%':
-			calObj.operation = butName;
-			break;
-		case '=':
-			return total;
+  const calObj = {
+    total,
+    next,
+    operation,
+  };
+  switch (butName) {
+    case 'AC':
+      calObj.total = '';
+      calObj.next = '';
+      calObj.operation = '';
+      break;
+    case '+/-':
+      calObj.total *= -1;
+      calObj.next *= -1;
+      break;
+    case '+':
+    case '-':
+    case 'x':
+    case '÷':
+    case '%':
+      calObj.operation = butName;
+      break;
+    case '=':
+      return total;
 
-		default:
-			return calObj
-	}
-	return calObj;
-}
+    default:
+      return calObj;
+  }
+  return calObj;
+};
 export default calculate;
