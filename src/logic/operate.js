238946d1f7c74b711766558f5662f6ef1dfe872a
addo <forison6205@gmail.com>
2019-12-18 08:54:32 -0500

    fix: linter error

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,27 +1,27 @@
-import big from 'big.js';
+import big from 'big';
 
 const operate = (numberOne, numberTwo, operation) => {
-  let results;
-  switch (operation) {
-    case '+':
-      results = big(numberOne).plus(numberTwo).toString();
-      break;
-    case '-':
-      results = big(numberOne).minus(numberTwo).toString();
-      break;
-    case 'x':
-      results = big(numberOne).times(numberTwo).toString();
-      break;
-    case '÷':
-      results = numberTwo === 0 ? 'undefined' : big(numberOne).div(numberTwo).toString();
-      break;
-    case '%':
-      results = big(numberOne).div(100).toString();
-      break;
-    default:
-      results = 'invalid operator';
-      break;
-  }
-  return results;
+	let results;
+	switch (operation) {
+		case '+':
+			results = big(numberOne).plus(numberTwo).toString();
+			break;
+		case '-':
+			results = big(numberOne).minus(numberTwo).toString();
+			break;
+		case 'x':
+			results = big(numberOne).times(numberTwo).toString();
+			break;
+		case '÷':
+			results = numberTwo === 0 ? 'undefined' : big(numberOne).div(numberTwo).toString();
+			break;
+		case '%':
+			results = big(numberOne).div(100).toString();
+			break;
+		default:
+			results = 'invalid operator';
+			break;
+	}
+	return results;
 };
 export default operate;
