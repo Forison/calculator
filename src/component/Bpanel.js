eff04bf4e424778cf4f05417122c4881581e1367
addo <forison6205@gmail.com>
2020-03-23 18:26:16 -0400

    add advanced functionalities

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -5,6 +5,7 @@ import Button from './Button';
 
 const wide = arg => (arg === '0' ? 'wide' : 'normal');
 const zeroStyling = arg => (arg === '0' ? 'col-6' : 'col-3');
+const advStyling = 'col-2';
 const colorStyler = () => ('colorMe');
 const Bpanel = ({ clickHandler }) => {
   const butName = [
@@ -14,7 +15,13 @@ const Bpanel = ({ clickHandler }) => {
     ['1', '2', '3', '+'],
     ['0', '.', '='],
   ];
-
+  const butAdvance = [
+    ['(', ')', 'mc', 'm+', 'm-', 'mr'],
+    ['2^nd', 'x^2', 'x^3', 'x^y', 'e^x', '10^x'],
+    ['1/x', '2√x', '3√x', 'y√x', 'ln', 'log10'],
+    ['x!', 'sin', 'cos', 'tan', 'e', 'EE'],
+    ['Rad', 'sinh', 'cosh', 'tanh', 'π', 'Rand'],
+  ];
   return (
     <div>
       {
@@ -23,7 +30,23 @@ const Bpanel = ({ clickHandler }) => {
             <div className="container">
               <div className="row">
                 {group.map(ButtonText => (
-                  <div key={id.generate()} className={`${zeroStyling(ButtonText)}`}>
+                  <div key={id.generate()} className={`${zeroStyling(ButtonText)} `}>
+                    <Button name={ButtonText} color={colorStyler()} wide={`${wide(ButtonText)}`} clickMe={clickHandler} />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))
+      }
+
+      {
+        butAdvance.map(group => (
+          <div key={id.generate()}>
+            <div className="container">
+              <div className="row">
+                {group.map(ButtonText => (
+                  <div key={id.generate()} className={`${advStyling} p-0`}>
                     <Button name={ButtonText} color={colorStyler()} wide={`${wide(ButtonText)}`} clickMe={clickHandler} />
                   </div>
                 ))}
