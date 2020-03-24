885c4b48d387707b3cecf26e7703a616e9e0cd47
addo <forison6205@gmail.com>
2020-03-24 05:18:23 -0400

    add advance method

--- src/component/Bpanel.js
+++ src/component/Bpanel.js
@@ -4,23 +4,22 @@ import propTypes from 'prop-types';
 import Button from './Button';
 
 const wide = arg => (arg === '0' ? 'wide' : 'normal');
-const zeroStyling = arg => (arg === '0' ? 'col-6' : 'col-3');
-const advStyling = 'col-2';
+const zeroStyling = arg => (arg === '0' ? 'col-6' : 'col-2 p-0');
+const advStyling = (arg) => {
+  if (arg === '=') {
+    return 'col-4'
+  }
+};
 const colorStyler = () => ('colorMe');
 const Bpanel = ({ clickHandler }) => {
+  let advCal = false;
   const butName = [
-    ['AC', '+/-', '%', '÷'],
-    ['7', '8', '9', 'x'],
-    ['4', '5', '6', '-'],
-    ['1', '2', '3', '+'],
+    ['AC', '+/-', '%', '÷', 'tan', 'log'],
+    ['7', '8', '9', 'x', 'ln', 'e'],
+    ['4', '5', '6', '-', '√', 'Exp'],
+    ['1', '2', '3', '+', , ')', '('],
+    ['Rad', 'π', 'x!', 'Inv', 'sin', 'Cos'],
     ['0', '.', '='],
-  ];
-  const butAdvance = [
-    ['(', ')', 'mc', 'm+', 'm-', 'mr'],
-    ['2^nd', 'x^2', 'x^3', 'x^y', 'e^x', '10^x'],
-    ['1/x', '2√x', '3√x', 'y√x', 'ln', 'log10'],
-    ['x!', 'sin', 'cos', 'tan', 'e', 'EE'],
-    ['Rad', 'sinh', 'cosh', 'tanh', 'π', 'Rand'],
   ];
   return (
     <div>
@@ -30,7 +29,7 @@ const Bpanel = ({ clickHandler }) => {
             <div className="container">
               <div className="row">
                 {group.map(ButtonText => (
-                  <div key={id.generate()} className={`${zeroStyling(ButtonText)} `}>
+                  <div key={id.generate()} className={`${zeroStyling(ButtonText)} ${advStyling(ButtonText)}`}>
                     <Button name={ButtonText} color={colorStyler()} wide={`${wide(ButtonText)}`} clickMe={clickHandler} />
                   </div>
                 ))}
@@ -39,23 +38,7 @@ const Bpanel = ({ clickHandler }) => {
           </div>
         ))
       }
-
-      {
-        butAdvance.map(group => (
-          <div key={id.generate()}>
-            <div className="container">
-              <div className="row">
-                {group.map(ButtonText => (
-                  <div key={id.generate()} className={`${advStyling} p-0`}>
-                    <Button name={ButtonText} color={colorStyler()} wide={`${wide(ButtonText)}`} clickMe={clickHandler} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        ))
-      }
-    </div>
+    </div >
   );
 };
 
